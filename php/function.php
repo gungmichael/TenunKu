b3d9ec184b735fc3209910902b6e2d663864e225
Michael Swisnandya <61909398+gungmichael@users.noreply.github.com>
2022-12-23 01:42:53 +0800

    tambahan ges

--- php/function.php
+++ php/function.php
@@ -18,7 +18,56 @@ function login()
             exit;
         }
     }
-
     return false;
+}
+
+function adminLogin()
+{
+    global $conn;
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = mysqli_query($conn, "SELECT * FROM admin WHERE email = '$email'");
+
+    if (mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['adminLogin'] = true;
+            header("Location: index.php");
+            exit;
+        }
+    }
+    return false;
+}
+
+function register()
+{
+    global $conn;
+
+    $username = $_POST['username']
+    $nama = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email'");
+
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+                alert('Username sudah terdaftar');
+            </script>";
+        return false;
+    }
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO user VALUES (null, '$username', '$password', '$email', '$nama')";
+    $resultUser = mysqli_query($conn, $query);
+
+    if ($resultUser) {
+        header("Location: login.php");
+    } else {
+        echo "Gagal menambahkan data";
+    }
 }
 ?>
