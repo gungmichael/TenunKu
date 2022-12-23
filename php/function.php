370ae74e3150c12b2c4e2047530ea56dbcaf9618
Michael Swisnandya <61909398+gungmichael@users.noreply.github.com>
2022-12-23 14:04:21 +0800

    update tambahan, CRUD bagian dasbor

--- php/function.php
+++ php/function.php
@@ -1,8 +1,7 @@
 <?php
 include "config.php";
 
-function login()
-{
+function login(){
     global $conn;
 
     $email = $_POST['email'];
@@ -21,8 +20,7 @@ function login()
     return false;
 }
 
-function adminLogin()
-{
+function adminLogin(){
     global $conn;
 
     $email = $_POST['email'];
@@ -41,8 +39,7 @@ function adminLogin()
     return false;
 }
 
-function register()
-{
+function register(){
     global $conn;
 
     $username = $_POST['username'];
@@ -66,6 +63,66 @@ function register()
 
     if ($resultUser) {
         header("Location: login.php");
+    } else {
+        echo "Gagal menambahkan data";
+    }
+}
+
+function getItems(){
+    global $conn;
+
+    $query = "SELECT * FROM barang";
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $rows[] = $row;
+    }
+
+    if($result){
+        return $rows;
+    } else {
+        return false;
+    }
+}
+
+function getItemsbyID($id_barang){
+    global $conn;
+
+    $query = "SELECT * FROM barang WHERE id_barang = $id_barang";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+
+    if ($result) {
+        return $row;
+    } else {
+        return false;
+    }
+}
+
+function addItems()
+{
+    if (isset($_SESSION['adminLogin'])) {
+        if ($_SESSION['adminLogin'] == false) {
+            header("Location: admin_login.php");
+            return false;
+        }
+    } else {
+        header("Location: admin_login.php");
+        return false;
+    }
+
+    global $conn;
+
+    $id_barang = $_POST['id_barang'];
+    $nama_barang = $_POST['nama_barang'];
+    $qty_barang = $_POST['qty_barang'];
+    $jenis_barang = $_POST['jenis_barang'];
+    $keterangan = $_POST['keterangan'];
+    $query = "INSERT INTO barang (id_barang, nama_barang, qty_barang, jenis_barang, keterangan) VALUES ('$id_barang', '$nama_barang', '$qty_barang', '$jenis_barang', '$keterangan')";
+
+    $result = mysqli_query($conn, $query);
+    if ($result) {
+        header("Location: dashboard.php");
     } else {
         echo "Gagal menambahkan data";
     }
