a7dba2e8a5193f4d79432ea83488d84783d749e5
Michael Swisnandya <61909398+gungmichael@users.noreply.github.com>
2022-12-24 13:20:34 +0800

    terbaru, dash aman

--- php/function.php
+++ php/function.php
@@ -1,57 +1,10 @@
 <?php
 include "config.php";
 
-function login()
-{
-    global $conn;
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $result = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email'");
-
-    if (mysqli_num_rows($result) === 1) {
-        $row = mysqli_fetch_assoc($result);
-        if (password_verify($password, $row['password'])) {
-            $_SESSION['login'] = true;
-            header("Location: index.php");
-            exit;
-        }
-    }
-    return false;
-}
-
 function logout()
 {
     session_destroy();
     header("Location: login.php");
-    exit;
-}
-
-function adminLogin()
-{
-    global $conn;
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $result = mysqli_query($conn, "SELECT * FROM admins WHERE email = '$email'");
-
-    if (mysqli_num_rows($result) === 1) {
-        $row = mysqli_fetch_assoc($result);
-        if (password_verify($password, $row['password'])) {
-            $_SESSION['adminLogin'] = true;
-            header("Location: dash.php");
-            exit;
-        }
-    }
-    return false;
-}
-
-function logoutAdmin()
-{
-    session_destroy();
-    header("Location: admin_login.php");
     exit;
 }
 
